f0b3d9cca232c7ae9a76e6d52b8e4fcd19889ed0
Your Name <you@example.com>
2018-05-13 16:12:17 -0700

    updated with style and JS links

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,47 @@
+var game = ["a", "c", "e", "g", "i", "k", "m", "o"]
+    
+    var wins= 0;
+    var losses= 0;
+    var guessesLeft= 10;
+    var guesses= " ";
+    var userGuess = [];
+    var guessList=[]
+    
+    
+    
+   
+   var randomItem = game[Math.floor(Math.random() * game.length)];
+   console.log (randomItem);
+
+
+
+     document.onkeyup = function (event) {    
+           userGuess = event.key; console.log(userGuess)
+           guessList.push(userGuess); 
+           document.getElementById("guess-so-far").innerHTML= guessList;
+     
+    
+          if (userGuess == randomItem) {
+     wins++
+     document.getElementById("winss").innerHTML=wins;
+    
+        }
+    else{
+        
+            guessesLeft--;
+        document.getElementById("guess-left").innerHTML= guessesLeft;
+
+         if(guessesLeft ===0){
+    alert("You loose!")
+    }
+    if(guessesLeft < 1){
+            losses++;
+                document.getElementById("lose").innerHTML=losses;
+               
+        }
+       
+}
+     }
+function restart() {
+    location.reload();
+}
